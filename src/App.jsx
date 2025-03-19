c9e48e2ce2ea148a84eac990ac0dfad0caf2f781
yohei-tarutani <youheitarutani@gmail.com>
2025-03-18 19:12:43 -0700

    Initial settings and more

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="">
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
+};
+
+export default App;
